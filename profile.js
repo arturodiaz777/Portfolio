e103b0fd2a426496d89880e69b05aedf96d6aeac
Arturo Diaz <luisarturodiazmeza_abc@hotmail.com>
2021-06-16 23:40:14 -0500

    fifthcommit

--- profile.js
+++ profile.js
@@ -27,42 +27,25 @@ export const projects = [
   {
     name: "Books App",
     description:
-      "Una app fullstack para guardar datos de libros en una base de datos en la nube",
+      "An app fullstack to save books on the cloud",
     image: "portfolio1.jpg",
     url: "https://books-app-fullstack.herokuapp.com",
   },
   {
-    name: "Rest Api con nodejs",
+    name: "Rest Api",
     description:
-      "Una app fullstack para guardar datos de libros en una base de datos en la nube",
+      "A rest api maked with Nodejs",
     image: "portfolio2.jpg",
+    url: "https://github.com/arturodiaz777/restapi-nodejs",
+  },
+  {
+    name: "CRUD",
+    description:
+      "A crud about tasks maked with Nodejs and Mongodb",
+    image: "portfolio3.jpg",
     url: "https://github.com/arturodiaz777/restapi-nodejs",
   }
   
   
 ];
 
-
-export const posts = [
-  {
-    title: "React",
-    content:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
-    imageURL:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
-  },
-  {
-    title: "Angular",
-    content:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
-    imageURL:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
-  },
-  {
-    title: "Nextjs",
-    content:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
-    imageURL:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
-  },
-];
